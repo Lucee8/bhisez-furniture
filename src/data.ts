8599ccf766414f796e06a0321f67bf434a0d3c95
Lucee8 <luceecode@gmail.com>
2026-06-03 00:43:57 +0530

    update

--- src/data.ts
+++ src/data.ts
@@ -199,10 +199,10 @@ const LOCAL_IMAGES: Record<string, string[]> = {
 
   // BEDS
   "beds-premium-bed": [
-    "/images/BEDS/premium bed 01.WebP",
-    "/images/BEDS/premium bed 02.WebP",
-    "/images/BEDS/premium bed 03.WebP",
-    "/images/BEDS/premium bed 04.WebP",
+    "images/BEDS/premium bed 01.WebP",
+    "images/BEDS/premium bed 02.WebP",
+    "images/BEDS/premium bed 03.WebP",
+    "images/BEDS/premium bed 04.WebP",
   ],
   "beds-open-bed": [
     "/images/BEDS/open bed 01.WebP",
